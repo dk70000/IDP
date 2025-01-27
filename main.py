51edabb1218a6aa16aab0041ad3a994cbc60327f
Danny <dk700@cam.ac.uk>
2025-01-27 15:06:51 +0000

    Testing importing from other files

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 #this is the main file to be run by picobot
+from main import printroutes
+
+printroutes()
--- navigation.py
+++ navigation.py
@@ -1,1 +1,18 @@
 #this file contains all the navigation functions
+
+
+#refer to start as S, depots and 1 and 2, destinations as A-D. S1 would then be the route S to 1. L is left, R is right, N is null.
+#this contains the routes to take to and from each position
+routes = {"S1":"RR",
+          "S2":"LNL",
+          "1A":"LNR",
+          "1B":"NLL",
+          "1C":"NLNRL",
+          "1D":"NNLL",
+          "2A":"RL",
+          "2B":"NRNR",
+          "2C":"NRLL",
+          "2D":"NNRNR"}
+
+def printroutes():
+    print(routes)
