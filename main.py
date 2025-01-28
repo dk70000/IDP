f2fe5ebbf6f8af673fa789b2dad4c4cf77650ac6
Danny <dk700@cam.ac.uk>
2025-01-28 09:46:52 +0000

    Added actuator motor class. Made file for camera functions.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #this is the main file to be run by picobot
-import navigation
+from navigation import cornering, linefollowerbasic, routes
 from sensors import getIRsensorvalue,getbutton
 
 #overall loop to always run while on
@@ -9,9 +9,15 @@ while True:
     while getbutton() == 0:
         pass
     
+    currentcorner = 0
+    currentroute = "S1"
+
     #loop with actual function in it
     while getbutton() == 0:
         #this is the main loop that will run throughout
         IRvalues = [getIRsensorvalue(1), getIRsensorvalue(2), getIRsensorvalue(3), getIRsensorvalue(4)]
 
-    
+
+
+        if IRvalues[0] or IRvalues[3]:
+            1
--- motors.py
+++ motors.py
@@ -25,6 +25,23 @@ class Motor:
         self.m1Dir.value(1)
         self.pwm1.duty_u16(int(65535*speed/100))
 
+class ActuatorMotor:
+    def __init__(self):
+        pins = [3,2]  
+        self.m1Dir = Pin(pins[0] , Pin.OUT) # set motor direction
+        self.pwm1 = PWM(Pin(pins[1])) # set speed
+        self.pwm1.freq(1000) # set max frequency
+        self.pwm1.duty_u16(0) # set duty cycle
+    def off(self):
+        self.pwm1.duty_u16(0)
+    def Forward(self,speed):
+        self.m1Dir.value(0) # forward = 0 reverse = 1 motor 1
+        self.pwm1.duty_u16(int(65535*speed/100)) # speed range 0-100 motor 1
+    def Reverse(self,speed):
+        self.m1Dir.value(1)
+        self.pwm1.duty_u16(int(65535*speed/100))
+
 #motor 3 is left, motor 4 is right
 motor3=Motor(3)
 motor4=Motor(4)
+motor2=ActuatorMotor()
