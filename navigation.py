01a1075b6dd55b872ed03d2911fca7db7e97531c
Danny <dk700@cam.ac.uk>
2025-02-04 16:38:14 +0000

    added button as kill switch to all while loops in nav

--- navigation.py
+++ navigation.py
@@ -1,7 +1,7 @@
 #this file contains all the navigation functions
 from motors import motor2, motor3, motor4
 from utime import sleep_ms, ticks_ms, ticks_diff
-from sensors import Line2, Line3, IRdistancesensor
+from sensors import Line2, Line3, IRdistancesensor, button
 from camera import getroutefromblock
 
 #refer to start as S, depots and 1 and 2, destinations as A-D. S1 would then be the route S to 1. L is left, R is right, N is null.
@@ -101,7 +101,7 @@ def cornering(direction, speed):
         sleep_ms(initialturntime)
 
         #now wait until sensor hits line again
-        while Line3.value() == 0:
+        while (Line3.value() == 0) and (button.value() == 0):
             pass
 
     if direction == "R":
@@ -111,7 +111,7 @@ def cornering(direction, speed):
         sleep_ms(initialturntime)
 
         #now wait until sensor hits line again
-        while Line2.value() == 0:
+        while (Line2.value() == 0) and (button.value() == 0):
             pass
 
     motor3.off()
@@ -128,12 +128,12 @@ def panic():
     linefound = 0
     turns = 1
 
-    while linefound == 0:
+    while (linefound == 0) and (button.value() == 0):
         
         motor3.Forward(50)
         motor4.Reverse(50)
 
-        while ticks_diff(ticks_ms(), timer) < time * turns:
+        while (ticks_diff(ticks_ms(), timer) < time * turns) and (button.value() == 0):
             if Line3.value() == 1:
                 motor3.off()
                 motor4.off()
@@ -148,7 +148,7 @@ def panic():
         motor3.Reverse(50)
         motor4.Forward(50)
 
-        while ticks_diff(ticks_ms(), timer) < time * turns:
+        while (ticks_diff(ticks_ms(), timer) < time * turns) and (button.value() == 0):
             if Line2.value() == 1:
                 motor3.off()
                 motor4.off()
@@ -172,16 +172,16 @@ def blockpickup(depot):
 
     # Do line following for a fixed amount of time to get straight
     start = ticks_ms()
-    while ticks_diff(ticks_ms(), start) < STRAIGHTEN_TIME:
+    while (ticks_diff(ticks_ms(), start) < STRAIGHTEN_TIME) and (button.value() == 0):
         linefollowerbasic(50)
     
     # Move to 30cm from the block
     try:
         if IRdistancesensor.ping() >= QR_CODE_IDEAL_DISTANCE:
-            while(IRdistancesensor.ping() > QR_CODE_IDEAL_DISTANCE):
+            while (IRdistancesensor.ping() > QR_CODE_IDEAL_DISTANCE) and (button.value() == 0):
                 linefollowerbasic(50)
         elif IRdistancesensor.ping() < QR_CODE_MIN_DISTANCE:
-            while(IRdistancesensor.ping() < QR_CODE_MIN_DISTANCE):
+            while ((IRdistancesensor.ping() < QR_CODE_MIN_DISTANCE)) and (button.value() == 0):
                 motor3.Reverse(30)
                 motor4.Reverse(30)
             QR_CODE_ATTEMPTS = 1    # Don't try and move forward after failed QR code read since it's already too close to the block
@@ -198,14 +198,14 @@ def blockpickup(depot):
             pass
         else:
             start = ticks_ms()      # Move forward to try and read again from closer
-            while ticks_diff(ticks_ms(), start) < REREAD_MOVE_TIME:
+            while (ticks_diff(ticks_ms(), start) < REREAD_MOVE_TIME) and (button.value() == 0):
                 linefollowerbasic(50)
     
     if newdestination == None:
         return "A"
     
     # Move up to the 20mm from the block and a little bit further
-    while(IRdistancesensor.ping() > MIN_IR_RANGE):
+    while (IRdistancesensor.ping() > MIN_IR_RANGE) and (button.value() == 0):
         linefollowerbasic(50)
     sleep_ms(TIME_PAST_RANGE)
 
@@ -234,7 +234,7 @@ def blockdrop():
 
     #go forward for an amount of time following the line to make sure inside zone
     start = ticks_ms()
-    while ticks_diff(ticks_ms(), start) < forwardtime:
+    while (ticks_diff(ticks_ms(), start) < forwardtime) and (button.value() == 0):
         linefollowerbasic(50)
 
     #put down block
